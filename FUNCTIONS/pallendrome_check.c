ff132f610fc6c04dba3a8791cd9d09cb134bb720
Deep Mandal <deepmandal9091@gmail.com>
2024-12-06 22:11:02 +0530

    Add files via upload

---
+++ FUNCTIONS/pallendrome_check.c
@@ -0,0 +1,21 @@
+// check pallendrome - optimized way
+#include<stdio.h>
+#include<string.h>
+void isPallendrome(char*);//function declare
+int main(){
+    char str[10];
+    printf("Enter number or character : ");
+    scanf("%s",str);
+    isPallendrome(str);//fuction call
+    return 0;
+}
+void isPallendrome(char str[]){//function define
+    int i,j;
+    j = strlen(str)-1;
+    for(i=0;i<j;i++,j--)
+        if(str[i]!=str[j]){
+            printf("Not pallendrome\n");
+            return ;
+        }
+    printf("Pallendrome\n");
+}
