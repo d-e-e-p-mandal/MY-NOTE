f8e569831f6c23d964d384ee684063f6b97cf24c
Deep Mandal <deepmandal9091@gmail.com>
2024-12-20 22:18:31 +0530

    Add files via upload

---
+++ RECURSION/001_factiorial_number.c
@@ -0,0 +1,14 @@
+// Factirorial using recursion
+#include<stdio.h>
+int factorial(int n){
+    if(n==0 || n==1)
+        return 1;
+    return n*factorial(n-1);
+}
+int main(){
+    int n;
+    printf("Enter number : ");
+    scanf("%d",&n);
+    printf("Factiorial of %d is %d \n",n,factorial(n));
+    return 0;
+}
