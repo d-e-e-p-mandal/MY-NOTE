1fa8507a296a5159088024428f04e7316c73a96c
“Deep <deepmandal9091@gmail.com>
2025-06-14 22:31:12 +0530

    add

--- 006 WEB DEVELOPMENT/007 NODE JS/021_static/index.js
+++ 006 WEB DEVELOPMENT/007 NODE JS/021_static/index.js
@@ -1,41 +1,34 @@
+// const express = require("express");
+// const path = require("path");
 import express from "express";
-const app = express();
-
+import path, { dirname } from "path";
 import { fileURLToPath } from "url";
-import path from "path";
 import fs from "fs";
 import { readFile } from "fs/promises";
+const app = express();
 
-let port = 3000;
-
-
-// For __dirname in ES modules:
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
+let port = 3000;
 app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine","ejs");
-app.set("views", path.join(__dirname, "/views"));
+app.set("views", path.join(__dirname, "views"));
 
-//for static file
-
-//  app.use(express.static("public"));
 
 app.listen(port,()=>{
-    console.log("sever starting............. exit for (mac : control+c )");
-    
+    console.log("server started.........");
 });
-app.get("/ig/:username",async (req,res)=>{
-    let {username} = req.params;
-    console.log(username);
-    // const instaData = require("./data.json");
-    // const dataRaw = fs.readFileSync(path.join(__dirname, "data.json"), "utf-8");
-    // const instaData = JSON.parse(dataRaw);
 
+app.get("/:username",async(req,res)=>{
+    let {username} = req.params;
+    // let dataRaw = require("./data.json");
+    //  const dataRaw = fs.readFileSync(path.join(__dirname, "data.json"), "utf-8");
+    //  const instaData = JSON.parse(dataRaw);
     const dataRaw = await readFile(path.join(__dirname, "data.json"), "utf-8");
     const instaData = JSON.parse(dataRaw);
-    let data = instaData[username];
 
-    console.log(data);
-    res.render("insta_profile.ejs",{data});
+    let data = instaData[username];
+    res.render("home.ejs",{data});
 });
